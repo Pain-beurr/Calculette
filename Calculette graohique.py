bd0f47d81a0f9ed500a5c8a9a35a1501e1214185
Pain-beurr <baptistecasamatta@gmail.com>
2024-10-24 11:38:36 +0200

    hgfdskjds

--- Calculette graohique.py
+++ Calculette graohique.py
@@ -1,1 +1,16 @@
-import tkiteasy
+from tkiteasy import *
+
+largeur = 800
+longueur = 700
+
+class Calculette:
+
+    def __init__(self):
+        self.g = ouvrirFenetre(longueur, largeur)
+        self.g.attendreClic()
+
+        #def initGraphique(self):
+        self.g.afficherTexte("1",jhg)
+
+
+c = Calculette()
