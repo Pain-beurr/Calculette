2befde4986ba56a56027fb0e2138c7187453319c
Elmi96 <charifelmourabiti@gmail.com>
2024-10-28 00:53:59 +0100

    braaaa

--- Calculette graohique.py
+++ Calculette graohique.py
@@ -48,72 +48,87 @@ class Calculette:
         while clic != True:
             val1 = ''
             val2 = ''
-            chiffres = []
+            self.chiffres = []
             click = self.g.attendreClic()
 
             # Ligne 1 (Fonctions)
             if 500 < click.x < 580 and 160 < click.y < 240:
                 operateur = "/"
+                self.chiffres.append(str(operateur))
 
             # Ligne 2 (7, 8, 9, x)
             if 50 < click.x < 130 and 280 < click.y < 360:
                 chiffre = 7
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 200 < click.x < 280 and 280 < click.y < 360:
                 chiffre = 8
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 350 < click.x < 430 and 280 < click.y < 360:
                 chiffre = 9
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 500 < click.x < 580 and 280 < click.y < 360:
                 operateur = "x"
+                self.chiffres.append(str(operateur))
 
             # Ligne 3 (4, 5, 6, -)
             if 50 < click.x < 130 and 400 < click.y < 480:
                 chiffre = 4
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 200 < click.x < 280 and 400 < click.y < 480:
                 chiffre = 5
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 350 < click.x < 430 and 400 < click.y < 480:
                 chiffre = 6
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 500 < click.x < 580 and 400 < click.y < 480:
                 operateur = "-"
+                self.chiffres.append(str(operateur))
 
             # Ligne 4 (1, 2, 3, +)
             if 50 < click.x < 130 and 520 < click.y < 600:
                 chiffre = 1
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 200 < click.x < 280 and 520 < click.y < 600:
                 chiffre = 2
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 350 < click.x < 430 and 520 < click.y < 600:
                 chiffre = 3
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 500 < click.x < 580 and 520 < click.y < 600:
                 operateur = "+"
+                self.chiffres.append(str(operateur))
 
             # Ligne 5 (0, ., =)
             if 50 < click.x < 130 and 640 < click.y < 720:
                 fonction = "AC"
+                self.chiffres = []
+                val1 = ''
+                val2 = ''
             if 200 < click.x < 280 and 640 < click.y < 720:
                 chiffre = 0
-                chiffres.append(str(chiffre))
+                self.chiffres.append(str(chiffre))
                 val1 += str(chiffre)
             if 350 < click.x < 430 and 640 < click.y < 720:
                 fonction = "."
+                self.chiffres.append(str(fonction))
             if 500 < click.x < 580 and 640 < click.y < 720:
                 operateur = "="
+                self.operation()
+
+
+
+    def operation(self):
+        return None
+
 
 
 
