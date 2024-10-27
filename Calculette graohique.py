bc56bc5c94ed439ffdbc4ac50c6a32babcdbad72
Elmi96 <charifelmourabiti@gmail.com>
2024-10-28 00:40:39 +0100

    braaaa

--- Calculette graohique.py
+++ Calculette graohique.py
@@ -11,6 +11,7 @@ class Calculette:
         self.calcul()
         self.val1 = 0
         self.val2 = 0
+        self.aff = 0
 
     def initGraphique(self):
         # Ligne 1 (Fonctions)
@@ -43,54 +44,76 @@ class Calculette:
         self.g.attendreClic()
 
     def calcul(self):
-        val1 = ''
-        val2 = ''
         clic = False  # Boucle qui nous permet de faire tourner le programme a l'infini
         while clic != True:
+            val1 = ''
+            val2 = ''
             chiffres = []
-            click1 = self.g.attendreClic()
+            click = self.g.attendreClic()
 
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
-                chiffre = 1
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 60 < click1.x < 140 and 360 < click1.y < 440:
-                chiffre = 2
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 160 < click1.x < 220 and 360 < click1.y < 440:
-                chiffre = 3
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
-                chiffre = 4
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
-                chiffre = 5
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
-                chiffre = 6
-                chiffres.append(str(chiffre))
-                val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
+            # Ligne 1 (Fonctions)
+            if 500 < click.x < 580 and 160 < click.y < 240:
+                operateur = "/"
+
+            # Ligne 2 (7, 8, 9, x)
+            if 50 < click.x < 130 and 280 < click.y < 360:
                 chiffre = 7
                 chiffres.append(str(chiffre))
                 val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
+            if 200 < click.x < 280 and 280 < click.y < 360:
                 chiffre = 8
                 chiffres.append(str(chiffre))
                 val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
+            if 350 < click.x < 430 and 280 < click.y < 360:
                 chiffre = 9
                 chiffres.append(str(chiffre))
                 val1 += str(chiffre)
-            if 0 < click1.x < 60 and 360 < click1.y < 440:
+            if 500 < click.x < 580 and 280 < click.y < 360:
+                operateur = "x"
+
+            # Ligne 3 (4, 5, 6, -)
+            if 50 < click.x < 130 and 400 < click.y < 480:
+                chiffre = 4
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 200 < click.x < 280 and 400 < click.y < 480:
+                chiffre = 5
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 350 < click.x < 430 and 400 < click.y < 480:
+                chiffre = 6
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 500 < click.x < 580 and 400 < click.y < 480:
+                operateur = "-"
+
+            # Ligne 4 (1, 2, 3, +)
+            if 50 < click.x < 130 and 520 < click.y < 600:
+                chiffre = 1
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 200 < click.x < 280 and 520 < click.y < 600:
+                chiffre = 2
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 350 < click.x < 430 and 520 < click.y < 600:
+                chiffre = 3
+                chiffres.append(str(chiffre))
+                val1 += str(chiffre)
+            if 500 < click.x < 580 and 520 < click.y < 600:
+                operateur = "+"
+
+            # Ligne 5 (0, ., =)
+            if 50 < click.x < 130 and 640 < click.y < 720:
+                fonction = "AC"
+            if 200 < click.x < 280 and 640 < click.y < 720:
                 chiffre = 0
                 chiffres.append(str(chiffre))
                 val1 += str(chiffre)
-
+            if 350 < click.x < 430 and 640 < click.y < 720:
+                fonction = "."
+            if 500 < click.x < 580 and 640 < click.y < 720:
+                operateur = "="
 
 
 
